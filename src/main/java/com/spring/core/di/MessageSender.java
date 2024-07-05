254c6f898d8bf9751700ea415023bc281b0946e1
Truong Nguyen <vonguyen.0407@gmail.com>
2024-07-05 20:25:06 +0700

    026 Setter Based Dependency Injection

--- src/main/java/com/spring/core/di/MessageSender.java
+++ src/main/java/com/spring/core/di/MessageSender.java
@@ -10,17 +10,23 @@ public class MessageSender {
     private MessageService messageService;
     private MessageService eMessageService;
 
-    @Autowired
-    public MessageSender(MessageService messageService){
-        this.messageService = messageService;
-        System.out.println("contructor 1");
-    }
-
     // @Autowired
-    public MessageSender(@Qualifier("sms") MessageService messageService, MessageService eMessageService){
+    // public MessageSender(MessageService messageService){
+    //     this.messageService = messageService;
+    //     System.out.println("contructor 1");
+    // }
+
+    // // @Autowired
+    // public MessageSender(@Qualifier("sms") MessageService messageService, MessageService eMessageService){
+    //     this.messageService = messageService;
+    //     this.eMessageService = eMessageService;
+    //     System.out.println("contructor 2");
+    // }
+
+
+    @Autowired
+    public void setMessageSender(@Qualifier("sms") MessageService messageService){
         this.messageService = messageService;
-        this.eMessageService = eMessageService;
-        System.out.println("contructor 2");
     }
 
     public void sendMessage(String message){
