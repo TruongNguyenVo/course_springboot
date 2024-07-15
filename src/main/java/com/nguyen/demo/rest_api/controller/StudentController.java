76fefb6649a445e99b8d3f110476966e99774e29
Truong Nguyen <vonguyen.0407@gmail.com>
2024-07-15 20:21:23 +0700

    056 Spring Boot REST API with Path Variable - @PathVariable

--- src/main/java/com/nguyen/demo/rest_api/controller/StudentController.java
+++ src/main/java/com/nguyen/demo/rest_api/controller/StudentController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nguyen.demo.rest_api.bean.Student;
@@ -21,6 +22,7 @@ public class StudentController {
         return student;
     }
 
+    //http://localhost:8081/students
     @GetMapping("students")
     public List<Student> getStudents(){
         Student student1 = new Student(1, "nguyen", "vo");
@@ -28,8 +30,15 @@ public class StudentController {
         List<Student> students = new ArrayList<>();
         students.add(student1);
         students.add(student2);
+        students.add(new Student(3, "truong", "vo"));
         return students;
 
     }
 
+    //Spring Boot RestAPI with path varible
+    //http://localhost:8081/student/id=1&first_name=nguyen
+    @GetMapping("student/id={id}&first_name={first_name}")
+    public Student studentPathVariable(@PathVariable("id") int id, @PathVariable("first_name") String firstName){
+        return new Student(id, firstName, "vo");
+    }
 }
