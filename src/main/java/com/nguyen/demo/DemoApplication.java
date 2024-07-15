e82d8564765ef6434aee5027f14bb9c21d1772a3
Truong Nguyen <vonguyen.0407@gmail.com>
2024-07-15 19:41:27 +0700

    053 Create Simple Spring Boot REST API

--- src/main/java/com/nguyen/demo/DemoApplication.java
+++ src/main/java/com/nguyen/demo/DemoApplication.java
@@ -2,26 +2,17 @@ package com.nguyen.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class DemoApplication {
 
-	@Bean
-	public HelloWorld helloWorld(){
-		return new HelloWorld();
-	}
 	public static void main(String[] args) {
 		//http:localhost:8081
-		ApplicationContext applicationContext =  SpringApplication.run(DemoApplication.class, args);																																																																																																																																											
-		// HelloWorld helloWorld = applicationContext.getBean(HelloWorld.class);
-		// System.out.println(helloWorld.hello());
+		SpringApplication.run(DemoApplication.class, args);																																																																																																																																											
+	
 
-		String[] beanNames = applicationContext.getBeanDefinitionNames();
-		for (String bean : beanNames) {
-			System.out.println(bean);
-		}
+
 	}
 
 }
