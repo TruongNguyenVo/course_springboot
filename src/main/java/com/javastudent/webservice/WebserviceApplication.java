70db0f6f299ea5d92d53fb4d1d26f70d3e171f01
Truong Nguyen <vonguyen.0407@gmail.com>
2024-07-31 20:37:43 +0700

    118 Define General API Information using Annotations

--- src/main/java/com/javastudent/webservice/WebserviceApplication.java
+++ src/main/java/com/javastudent/webservice/WebserviceApplication.java
@@ -5,7 +5,33 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Spring Boot REST API Documentation",
+		description = "Sring Boot REST API Documentation",
+		version = "v1.0",
+		contact = @Contact(
+			name = "Vo Truong Nguyen",
+			email = "vonguyen.0407@gmail.com",
+			url = "https://github.com/TruongNguyenVo"
+		),
+		license = @License(
+			name = "Nguyen 1.0",
+			url = "https://github.com/TruongNguyenVo"
+		)
+	),
+	externalDocs = @ExternalDocumentation(
+		description = "Spring Boot User Management Documentation",
+		url = "https://github.com/TruongNguyenVo"
+	)
+)
 public class WebserviceApplication {
 	@Bean
 	public ModelMapper modelMapper(){
