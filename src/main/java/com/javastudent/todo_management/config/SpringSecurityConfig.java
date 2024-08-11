b85c5c46b836558a25c9811b7c74d3a2dc561fe2
Truong Nguyen <vonguyen.0407@gmail.com>
2024-08-11 21:38:25 +0700

    142

--- src/main/java/com/javastudent/todo_management/config/SpringSecurityConfig.java
+++ src/main/java/com/javastudent/todo_management/config/SpringSecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configurable
@@ -14,8 +17,21 @@ public class SpringSecurityConfig {
         http.csrf(csrf -> csrf.disable()).authorizeHttpRequests((authorize) -> {
             authorize.anyRequest().authenticated();
         }).httpBasic(Customizer.withDefaults());
-
-
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetails(){
+        UserDetails user = User.builder()
+                            .username("vonguyen")
+                            .password("1234")
+                            .roles("User")
+                            .build();
+        UserDetails admin = User.builder()
+                            .username("admin")
+                            .password("admin")
+                            .roles("ADMIN")
+                            .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
