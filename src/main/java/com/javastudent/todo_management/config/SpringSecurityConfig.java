a5be752fad00d8c7003de3e6ce98bc2c2b3adf25
Truong Nguyen <vonguyen.0407@gmail.com>
2024-08-19 20:33:27 +0700

    149 done

--- src/main/java/com/javastudent/todo_management/config/SpringSecurityConfig.java
+++ src/main/java/com/javastudent/todo_management/config/SpringSecurityConfig.java
@@ -2,20 +2,25 @@ package com.javastudent.todo_management.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.javastudent.todo_management.security.CustomUserDetailsService;
+
+import lombok.AllArgsConstructor;
 
 @Configuration
 @EnableMethodSecurity
+@AllArgsConstructor
 public class SpringSecurityConfig {
+    private CustomUserDetailsService customUserDetailsService;
+
     @Bean
     SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
         http.csrf(csrf -> csrf.disable()).authorizeHttpRequests((authorize) -> {
@@ -27,24 +32,32 @@ public class SpringSecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.builder()
-                                .username("nguyenvo")
-                                .password(passwordEncoder().encode("12345"))
-                                .roles("USER")
-                                .build();
-        UserDetails admin = User.builder()
-                                .username("admin")
-                                .password(passwordEncoder().encode("admin"))
-                                .roles("ADMIN")
-                                .build();
+    // @Bean
+    // public UserDetailsService userDetailsService() {
+    //     UserDetails user = User.builder()
+    //                             .username("nguyenvo")
+    //                             .password(passwordEncoder().encode("12345"))
+    //                             .roles("USER")
+    //                             .build();
+    //     UserDetails admin = User.builder()
+    //                             .username("admin")
+    //                             .password(passwordEncoder().encode("admin"))
+    //                             .roles("ADMIN")
+    //                             .build();
 
-        return new InMemoryUserDetailsManager(user, admin);
-    }
+    //     return new InMemoryUserDetailsManager(user, admin);
+    // }
+
+    
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
 }
