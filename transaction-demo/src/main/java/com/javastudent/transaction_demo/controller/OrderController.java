4325509ebf4fda51126fd41a4496d0322109bf0b
Truong Nguyen <vonguyen.0407@gmail.com>
2024-08-21 21:30:51 +0700

    161 create placeOrder RestAPI

---
+++ transaction-demo/src/main/java/com/javastudent/transaction_demo/controller/OrderController.java
@@ -0,0 +1,28 @@
+package com.javastudent.transaction_demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javastudent.transaction_demo.dto.OrderRequest;
+import com.javastudent.transaction_demo.dto.OrderResponse;
+import com.javastudent.transaction_demo.service.OrderService;
+
+
+@RestController
+@RequestMapping("/api/v1/orders")
+public class OrderController {
+    private OrderService orderService;
+
+    public OrderController(OrderService orderService){
+        this.orderService = orderService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<OrderResponse> placeOrder(@RequestBody OrderRequest orderRequest){
+        return ResponseEntity.ok(orderService.placeOrder(orderRequest));
+    }
+
+}
