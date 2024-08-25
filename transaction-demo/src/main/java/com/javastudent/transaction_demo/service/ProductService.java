2253ec25fd0bc769e50623d4ca05a69b6a3af8fd
Truong Nguyen <vonguyen.0407@gmail.com>
2024-08-25 19:49:03 +0700

    169 Create Service Layer

---
+++ transaction-demo/src/main/java/com/javastudent/transaction_demo/service/ProductService.java
@@ -0,0 +1,9 @@
+package com.javastudent.transaction_demo.service;
+
+import java.util.List;
+
+import com.javastudent.transaction_demo.entity.Product;
+
+public interface  ProductService {
+    List<Product> searchProducts(String query);
+}
---
+++ transaction-demo/src/main/java/com/javastudent/transaction_demo/service/impl/ProductServiceImpl.java
@@ -0,0 +1,25 @@
+package com.javastudent.transaction_demo.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.javastudent.transaction_demo.entity.Product;
+import com.javastudent.transaction_demo.repository.ProductRepository;
+import com.javastudent.transaction_demo.service.ProductService;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+
+    private ProductRepository productRepository;
+    public ProductServiceImpl(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public List<Product> searchProducts(String query) {
+        List<Product> products = productRepository.searchProducts(query);
+        return null;
+    }
+
+}
