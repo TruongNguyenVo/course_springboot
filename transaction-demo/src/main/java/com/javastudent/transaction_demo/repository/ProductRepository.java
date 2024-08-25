d8c4214ac2aacc03e57f0857d8374d90bb45de6b
Truong Nguyen <vonguyen.0407@gmail.com>
2024-08-25 19:37:51 +0700

    168 Create JPQL and Native SQL Queries to Search Products

--- transaction-demo/src/main/java/com/javastudent/transaction_demo/repository/ProductRepository.java
+++ transaction-demo/src/main/java/com/javastudent/transaction_demo/repository/ProductRepository.java
@@ -1,5 +1,20 @@
 package com.javastudent.transaction_demo.repository;
 
-public interface ProductRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.javastudent.transaction_demo.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+    @Query("SELECT p FROM Product p WHERE" +
+            "p.name LIKE CONCAT('%',: query, '%')" +
+            "OR p.description LIKE CONCAT('%', : query, '%')")
+    List<Product> searchProducts(String query);   
+
+    @Query(value="SELECT * FROM products p WHERE" +
+    "p.name LIKE CONCAT('%',: query, '%')" +
+    "OR p.description LIKE CONCAT('%', : query, '%')", nativeQuery=true)
+    List<Product> searchProductsSQL(String query);   
 }
